dbdbe603f4416789ac1ffe6524c09b2a29b2b2e4
edon92 <540324745@qq.com>
2018-06-15 17:13:55 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,8 +1,15 @@
 <template>
+	<div>
+		<home-header></home-header>
+	</div>
 </template>
-<script type="text/javascript">
-export default {
-  name: 'Test'
-}
+<script>
+	import HomeHeader from './components/Header'
+	export default {
+	  name: 'Home',
+	  components: {
+	  		HomeHeader
+	  }
+	}
 </script>
 <style type="text/css"></style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="header">
+		<div class="header-left">
+			<span class="iconfont icon-back">&#xe624;</span>
+		</div>
+		<div class="header-input" placeholder="请输入">
+		<span class="iconfont icon-input">&#xe632;</span>
+		输入城市/景点/游玩主题
+		</div>
+		<div class="header-right">
+			城市
+			<span class="iconfont icon-arrow">&#xe64a;</span>
+		</div>
+	</div>
+</template>
+<script type="text/javascript">
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style type='text/stylus' lang='stylus' scoped>
+	@import '~styles/varible.styl'
+	.header
+		display: flex
+		line-height .86rem
+		height: .86rem
+		background: $bgcolor
+		color: #fff
+	.header-left
+		width: .64rem	
+		float: left
+		text-align:center
+		.icon-back
+			height:	.86rem
+			font-size: .36rem
+			font-weight: bold
+			text-align: center			
+	.header-input
+		flex: 1
+		background: #fff
+		height: .6rem
+		line-height: .6rem
+		border-radius: .1rem
+		color:#ccc
+		margin: .12rem 0rem 0rem .2rem
+		padding-left: .2rem
+	.header-right
+		width: 1.24rem
+		float: right
+		text-align: center
+		.icon-arrow
+			font-size: .15rem
+			margin-left: -.05rem
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
