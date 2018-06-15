4fe97e5b0ac3c46d0e9e07f271152ce3d4b63243
edon92 <540324745@qq.com>
2018-06-16 01:58:26 +0800

    add Swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption" ref="mySwiper" >
+	    <!-- slides -->
+	    <swiper-slide v-for="item of swiperList" :key="item.id">
+	    	<img class="swiper-img" :src="item.imgUrl">
+	    </swiper-slide>
+	    <!-- Optional controls -->
+	    <div class="swiper-pagination"  slot="pagination"></div>
+	  </swiper>
+	</div>
+</template>
+<script>
+	import 'swiper/dist/css/swiper.css'
+	import { swiper, swiperSlide } from 'vue-awesome-swiper'
+	export default {
+	  name: 'Swiper',
+	  components: {
+	    swiper,
+	    swiperSlide
+	  },
+	  data (){
+	  	return {
+	  		swiperOption: {
+	  			pagination:'.swiper-pagination'
+	  		},
+	  		swiperList: [
+	  			{
+	  				id: '0001',
+	  				imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/fc/e47aa3e1c67bbc02.jpg_750x200_0f3eecf8.jpg'
+	  			},
+	  			{
+	  				id: '0002',
+	  				imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/11/947d8b7e221cbb02.jpg_750x200_4145c399.jpg'
+	  			}
+	  		]
+	  	}
+	  }
+	}
+</script>
+<style lang='stylus' scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+  		.swiper-img
+  			width: 100% 
+</style>
