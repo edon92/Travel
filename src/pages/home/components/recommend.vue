b26a9e5d31928c080709b2ea08c12793dce1e9a0
edon92 <540324745@qq.com>
2018-06-23 09:49:09 +0800

    add detail-banner

--- src/pages/home/components/recommend.vue
+++ src/pages/home/components/recommend.vue
@@ -6,14 +6,20 @@
         热门推荐
       </div>
       <ul class="item-list">
-        <li class="item border-bottom" v-for='item of recommendList' :key='item.id'>
+        <router-link
+         tag="li"
+         class="item border-bottom"
+         v-for='item of recommendList'
+         :key='item.id'
+         :to="'/detail/' + item.id"
+         >
           <img class="item-img" :src='item.imgUrl'>
           <div class="item-info">
             <p class="item-title">{{item.title}}</p>
             <p class="item-desc">{{item.desc}}</p>
             <button class="item-button">查看详情</button>
           </div>
-        </li>
+        </router-link>
       </ul>
     </div>
   </div>
